31e528bf2945d999d725227aa8442454f06bb62d
Amir <shahrabi527@gmail.com>
2025-08-25 22:31:09 +0330

    Adding category

---
+++ Blog.DataBase/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace Blog.DataBase.Entities;
+
+public class Category : BaseEntity
+{
+    public string Title { get; set; } = "";
+
+    public string Slug { get; set; } = "";
+
+    public string MetaTag { get; set; } = "";
+
+    public string MetaDescription { get; set; } = "";
+
+    public List<Post> Posts { get; set; } = [];
+
+    public List<Post> SubPost { get; set; } = [];
+
+}
