e223e6b08682b7f3b5f0089c3dfca969af75c220
Amir <shahrabi527@gmail.com>
2025-08-25 22:30:09 +0330

    Adding blog bootstrapper

--- Blog.Bottstrap/BlogBootstrapper.cs
+++ Blog.Bottstrap/BlogBootstrapper.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Blog.DataBase.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Blog.Bootstrap;
 
 public static class BlogBootstrapper
 {
-    public static void InitDependency(this IServiceCollection services,string connectionString)
+    public static void InitDependency(this IServiceCollection services, string connectionString)
     {
-        
+        #region context
+        services.AddDbContext<BlogContext>(op =>op.UseSqlServer(connectionString),contextLifetime: ServiceLifetime.Scoped);
+        #endregion
+
+
+
     }
 }
