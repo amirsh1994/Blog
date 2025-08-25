c3499e4b9271c4cdee3fe85c33c8bc1983f937ab
Amir <shahrabi527@gmail.com>
2025-08-25 22:30:43 +0330

    adding blog context

---
+++ Blog.DataBase/Context/BlogContext.cs
@@ -0,0 +1,33 @@
+﻿using Blog.DataBase.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Blog.DataBase.Context;
+
+public class BlogContext(DbContextOptions<BlogContext>options):DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        //foreach (IMutableForeignKey foreignKey in modelBuilder.Model.GetEntityTypes().SelectMany(x=>x.GetForeignKeys()))
+        //{
+        //    foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+        //}
+        base.OnModelCreating(modelBuilder);
+    }
+    public DbSet<Comment> Comments { get; set; }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Post> Posts { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<Role> Roles { get; set; }
+
+    public DbSet<UserRole> UserRoles { get; set; }
+
+    public DbSet<Permission> Permissions { get; set; }
+
+    public DbSet<RolePermission> RolePermissions { get; set; }
+}
---
+++ Blog.DataBase/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog.DataBase.Entities;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+
+    public DateTime? LastModified { get; set; }
+
+    public DateTime CreationDate { get; set; }=DateTime.Now;
+
+    public bool IsDeleted { get; set; }
+}
