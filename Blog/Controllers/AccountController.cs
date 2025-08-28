bcb94a7f345b4afc51e41c8def3abe7831e79f7e
Amir <shahrabi527@gmail.com>
2025-08-28 11:31:24 +0330

    complete loging form and register

---
+++ Blog/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers;
+
+public class AccountController:BaseController
+{
+    #region login
+    [HttpGet]
+    [Route("Login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    #endregion
+
+    #region register
+
+    [HttpGet]
+    [Route("Register")]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    #endregion
+}
+
